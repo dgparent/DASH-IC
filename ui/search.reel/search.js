1ca8c10488cf7aa38c152aab612479aeb49844db
dgparent <noreply>
2018-08-03 16:50:47 +0000

    Add component search

---
+++ ui/search.reel/search.js
@@ -0,0 +1,12 @@
+/**
+ * @module "ui/search.reel"
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Search
+ * @extends Component
+ */
+exports.Search = Component.specialize(/** @lends Search.prototype */{
+
+});
